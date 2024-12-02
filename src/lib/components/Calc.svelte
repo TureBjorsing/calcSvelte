59ed25095989d1f468ca65f04abf14e190e0eace
TureBjorsing <turebjorsing@gmail.com>
2024-12-02 10:08:46 +0100

    Klar

--- src/lib/components/Calc.svelte
+++ src/lib/components/Calc.svelte
@@ -1,15 +1,50 @@
 <script>
     import Keyboard from './Keyboard.svelte';
 
+    let num = 0;
+    let newNum = false;
+    let operatorOn = false;
+    let operator = "";
+
     let display = "";
     function updateDisplay(e) {
         let val = e.target.value; // Hämtar knappens värde
 
         if(val === "ac") { // Rensar displayen
             display = "";
+            operator = null;
+            num = 0;
         } else if(Number(val) >= 0 && Number(val) < 10 || val === ".") { // skriver in nummer
+            if(newNum) {
+                display = "";
+                newNum = false;
+            }
             display += val;
+        } else if(val == "=") {
+            calculate(operator);
+        } else {
+            num = parseFloat(display);
+            operator = val;
+            if(operatorOn) {
+                calculate(operator);
+            }
+            operatorOn = true;
+            newNum = true;
         }
+    }
+
+    function calculate(arithmetic) {
+        if(arithmetic == "+") {
+            display = num + parseFloat(display);
+        } else if(arithmetic == "-") {
+            display = num - parseFloat(display);
+        } else if(arithmetic == "x") {
+            display = num * parseFloat(display);
+        } else if(arithmetic == "/") {
+            display = num / parseFloat(display);
+        }
+
+        operatorOn = false;
     }
 </script>
 
@@ -17,3 +52,27 @@
     <input type="text" value={display} disabled>
     <Keyboard on:click = {updateDisplay}/>
 </main>
+
+<style lang="scss">
+    main {
+        input {
+            width: 100%;
+            font-size: 5rem;
+            margin-bottom:10px;
+            background-color: black;
+            color: rgba(0, 255, 0, 1);
+            text-align: right;
+            padding: 10px 4px 10px 4px;
+            font-family: lcd;
+            border: 1px solid yellow;
+            border-radius:4px;
+        }
+
+        input[disabled] {
+        -webkit-text-fill-color: rgba(0, 255, 0, 1);
+        opacity: 1;
+        -webkit-opacity: 1;
+        color: rgba(0, 255, 0, 1)
+        }
+    }
+</style>
--- src/lib/components/Keyboard.svelte
+++ src/lib/components/Keyboard.svelte
@@ -2,25 +2,34 @@
     <button value=7 on:click>7</button>
     <button value=8 on:click>8</button>
     <button value=9 on:click>9</button>
-    <button id='add'>+</button>
+    <button value="+" on:click>+</button>
 
     <button value=4 on:click>4</button>
     <button value=5 on:click>5</button>
     <button value=6 on:click>6</button>
-    <button id='sub'>-</button>
+    <button value="-" on:click>-</button>
 
     <button value=1 on:click>1</button>
     <button value=2 on:click>2</button>
     <button value=3 on:click>3</button>
-    <button id='mul'>x</button>
+    <button value="x" on:click>x</button>
     
     <button value="." on:click>,</button>
     <button value=0 on:click>0</button>
-    <button id='enter'>=</button>
-    <button id='div'>/</button>
+    <button value="=" on:click>=</button>
+    <button value="/" on:click>/</button>
     <button value=ac on:click style = 'grid-column: span 4;'>CLEAR</button>
 </section>
 
 <style lang="scss">
+    section {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-auto-rows: minmax(7rem, auto);
+        grid-gap: 0.2rem;
 
+        button {
+            font-size: 5rem;
+        }
+    }
 </style>
